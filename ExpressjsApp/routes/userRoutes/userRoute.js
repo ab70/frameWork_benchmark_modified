8ca5598c0c86fa40637b77e46b160d65c5d7e5ea
ab70 <nurulabrar2369@gmail.com>
2023-07-28 10:03:24 +0600

    chnage made

--- ExpressjsApp/routes/userRoutes/userRoute.js
+++ ExpressjsApp/routes/userRoutes/userRoute.js
@@ -2,6 +2,6 @@ const userController = require("../../app/controllers/userController")
 const express = require("express")
 const router = express.Router()
 
-router.get("/", userController().userTestController)
+router.get("/a", userController().userTestController)
 
 module.exports = router
